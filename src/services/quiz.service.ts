7343e44423ec1c24549b51386ac7ebbb2f109539
Vinh <narutovinh@hotmail.fr>
2022-02-08 08:13:48 +0100

    Fin exo 7

--- src/services/quiz.service.ts
+++ src/services/quiz.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Quiz } from '../models/quiz.model';
 import { QUIZ_LIST } from '../mocks/quiz-list.mock';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -16,15 +17,21 @@ export class QuizService {
     * The list of quiz.
     * The list is retrieved from the mock.
     */
-  private quizzes: Quiz[] = QUIZ_LIST;
+  //private quizzes: Quiz[] = QUIZ_LIST;
+  private quizzes: Quiz[] = [];
 
   /**
    * Observable which contains the list of the quiz.
    * Naming convention: Add '$' at the end of the variable name to highlight it as an Observable.
    */
-  public quizzes$: BehaviorSubject<Quiz[]> = new BehaviorSubject(QUIZ_LIST);
+  public quizzes$: BehaviorSubject<Quiz[]> = new BehaviorSubject(this.quizzes);
+  //public quizzes$: BehaviorSubject<Quiz[]> = new BehaviorSubject(QUIZ_LIST);
 
-  constructor() {
+  public URL: string = "https://raw.githubusercontent.com/NablaT/starter-quiz-two/master/mock-quiz.json";
+
+
+  constructor(private http: HttpClient) {
+    this.getQuizzes()
   }
 
   addQuiz(quiz: Quiz) {
@@ -39,4 +46,11 @@ export class QuizService {
     this.quizzes$.next(this.quizzes)
 
   }
+
+
+  getQuizzes() {
+    this.http.get<Quiz[]>(this.URL).subscribe(quizList => quizList.forEach(quiz => this.addQuiz(quiz)))
+    this.quizzes$.next(this.quizzes)
+  }
 }
+
